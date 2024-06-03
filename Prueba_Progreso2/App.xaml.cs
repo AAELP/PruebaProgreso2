06319f91ea63e2f83d0f2ffb34a365ffcc09daf6
Imsael <ismaelrobalino03@gmail.com>
2024-06-03 07:17:43 -0500

    Agregar archivos de proyecto.

---
+++ Prueba_Progreso2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Prueba_Progreso2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Prueba_Progreso2/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Prueba_Progreso2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
